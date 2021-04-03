8ea60edcaaf99162fc6b05e97916d82992c1c24d
michaelpaulkunz <MichaelPaulKunz@gmail.com>
2021-04-03 15:54:01 -0500

    map renders with hidden api key

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -4,6 +4,7 @@ import React from 'react';
 import Map from './Map.jsx';
 import Route from './Route.jsx';
 import Safety from './Safety.jsx';
+const { mapKey } = require('../../../server/config');
 
 class App extends React.Component {
   constructor(props) {
@@ -21,12 +22,12 @@ class App extends React.Component {
       <div>
         <Route />
         <Safety />
-        <div style={{width: '100vw', height: '100vh'}}>
+        <div style={{width: '50vw', height: '80vh'}}>
           <Map
-            googleMapURL={'https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places'}
-            loadingElement={<div style={{ height: '100%'}} />}
-            containerElement={<div style={{ height: '100%'}} />}
-            mapElement={<div style={{ height: '100%'}} />}
+            googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${mapKey}`}
+            loadingElement={<div style={{ height: '80%'}} />}
+            containerElement={<div style={{ height: '80%'}} />}
+            mapElement={<div style={{ height: '80%'}} />}
           />
         </div>
       </div>
--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -4,8 +4,8 @@ import { GoogleMap, withScriptjs, withGoogleMap } from 'react-google-maps';
 const WrapMe = function() {
   return (
     <GoogleMap
-      defaultZoom={10}
-      defaultCenter={{ lat: 45.421532, lng: -75.697189 }}
+      defaultZoom={4}
+      defaultCenter={{ lat: 37.0902, lng: -95.7129 }}
     />
   );
 };
