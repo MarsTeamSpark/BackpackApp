1300510a860f1f9fe593fbda5998d2612f1ef27d
michaelpaulkunz <MichaelPaulKunz@gmail.com>
2021-04-03 13:25:11 -0500

    map rendering, needs api key

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -19,9 +19,16 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Map/>
         <Route />
         <Safety />
+        <div style={{width: '100vw', height: '100vh'}}>
+          <Map
+            googleMapURL={'https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places'}
+            loadingElement={<div style={{ height: '100%'}} />}
+            containerElement={<div style={{ height: '100%'}} />}
+            mapElement={<div style={{ height: '100%'}} />}
+          />
+        </div>
       </div>
     );
   }
