85f8a2292028741615d891f12cd2719030e3df13
michaelpaulkunz <MichaelPaulKunz@gmail.com>
2021-04-05 10:44:26 -0500

    map renders list of national parks

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
-import { GoogleMap, withScriptjs, withGoogleMap } from 'react-google-maps';
+import { GoogleMap, withScriptjs, withGoogleMap, Marker } from 'react-google-maps';
 //getting started with render parks
+import * as parksData from '../../src/nationalparksdata.json';
 const WrapMe = function() {
   return (
     <GoogleMap
       defaultZoom={4}
       defaultCenter={{ lat: 37.0902, lng: -95.7129 }}
-    />
+    >
+      {parksData.map((park) => (
+        <Marker key={park.id} position={{
+          lat: park.coordinates.latitude,
+          lng: park.coordinates.longitude
+        }} />
+      ))}
+    </GoogleMap>
   );
 };
 
