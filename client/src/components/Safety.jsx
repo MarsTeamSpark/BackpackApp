cecd16bd62be8dc59e5d0ba3efc68ef43daa8e7c
Randolph Perkins <randyperkins2k@gmail.com>
2021-04-05 17:12:41 -0500

    Filled out Safety.jsx, passed in as child component to Information.jsx.

--- client/src/components/Safety.jsx
+++ client/src/components/Safety.jsx
@@ -1,25 +1,64 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-console */
 import React from 'react';
 // import PropTypes from 'prop-types';
 // import ReactDOM from 'react-dom';
-
+import axios from 'axios';
+const { rapidApiKey } = require('../../../server/config.js');
 class Safety extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      //searchInput: props.searchInput,
+      restrictions: '',
+      safetyIndex: '',
+      risk: ''
     };
 
   }
 
+  getSafetyData() {
+    //const { searchInput } = this.props;
+    const options = {
+      method: 'GET',
+      url: 'https://safe-travel-covid-index.p.rapidapi.com/safeindex',
+      params: {place: `${this.props.searchInput}, USA`, lang: 'en'},
+      headers: {
+        'x-rapidapi-key': rapidApiKey,
+        'x-rapidapi-host': 'safe-travel-covid-index.p.rapidapi.com'
+      }
+    };
 
+    axios.request(options).then((response) => {
+      console.log(response.data);
+      this.setState({
+        restrictions: response.data.restrictions,
+        safetyIndex: response.data.safety_index,
+        risk: response.data.risk
+      });
+    })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
+  // componentDidMount() {
+  //   this.getSafetyData();
+  // }
 
   render() {
     return (
-      <div style={{color: 'white'}}>
+      <div >
+        {console.log('From Safety.jsx:', this.props.searchInput)}
         <div className="Safety"> This is where safety functionality will come in </div>
+        <div>Restrictions: {this.state.restrictions}</div>
+        <div>Safety Index: {this.state.safetyIndex}</div>
+        <div>Risk: {this.state.risk}</div>
+        <button onClick={() => this.getSafetyData()}>Get Info</button>
       </div>
     );
   }
 }
 
 export default Safety;
+
