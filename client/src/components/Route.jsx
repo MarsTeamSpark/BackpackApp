e97e4d649fcd57f100ded7aae8f892c38bf89de2
michaelpaulkunz <MichaelPaulKunz@gmail.com>
2021-04-05 14:26:01 -0500

    rendering map from route

--- client/src/components/Route.jsx
+++ client/src/components/Route.jsx
@@ -8,7 +8,8 @@ import axios from 'axios';
 const { ORS_KEY } = require('../../../server/config.js');
 // import PropTypes from 'prop-types';
 // import ReactDOM from 'react-dom';
-
+import Map from './Map.jsx';
+const { mapKey } = require('../../../server/config');
 class Route extends React.Component {
   constructor(props) {
     super(props);
@@ -87,11 +88,11 @@ class Route extends React.Component {
   handleStartChange (e) {
     this.setState({startLocation: e.target.value});
   }
-  
+
   handleEndChange (e) {
     this.setState({endLocation: e.target.value});
   }
-  
+
   handleSearchChange (e) {
     this.setState({searchInput: e.target.value});
   }
@@ -115,7 +116,14 @@ class Route extends React.Component {
           <input type="text" name="end" className="input" placeholder="Choose Destination" onChange={handleEndChange}/>
           <button type="button" onClick={getRoute}>Get Route</button>
         </form>
-
+        <div style={{width: '50vw', height: '80vh'}}>
+          <Map
+            googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${mapKey}`}
+            loadingElement={<div style={{ height: '80%'}} />}
+            containerElement={<div style={{ height: '80%'}} />}
+            mapElement={<div style={{ height: '80%'}} />}
+          />
+        </div>
       </div>
     );
   }
