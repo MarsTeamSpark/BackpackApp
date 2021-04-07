72d14dc74f234cb4bcb58f6d2532ec8b9264f793
michaelpaulkunz <MichaelPaulKunz@gmail.com>
2021-04-07 16:27:41 -0500

    we may need to refactor here and use ejs

--- server/passport-setup.js
+++ server/passport-setup.js
@@ -4,6 +4,24 @@ const { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, GOOGLE_CALLBACK_URL} = require (
 
 const GoogleStrategy = require('passport-google-oauth2').Strategy;
 
+passport.serializeUser(function(user, done) {
+  /*
+  From the user take just the id (to minimize the cookie size) and just pass the id of the user
+  to the done callback
+  PS: You dont have to do it like this its just usually done like this
+  */
+  done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+  /*
+  Instead of user this function usually recives the id
+  then you use the id to select the user from the db and pass the user obj to the done callback
+  PS: You can later access this data in any routes in: req.user
+  */
+  done(null, user);
+});
+
 passport.use( new GoogleStrategy({
   clientID: GOOGLE_CLIENT_ID,
   clientSecret: GOOGLE_CLIENT_SECRET,
