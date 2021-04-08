92885d0e0a835fe14bd5fa835abf54707a9b4d14
michaelpaulkunz <MichaelPaulKunz@gmail.com>
2021-04-08 16:40:44 -0500

    moved app.jsx api calls to server

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 /* eslint-disable no-console */
+const axios = require('axios');
 require('dotenv').config();
 console.log(process.env.GOOGLE_CLIENT_ID);
+const ORS_KEY = process.env.ORS_KEY;
 const path = require('path');
 const express = require('express');
 const passport = require('passport');
@@ -44,7 +46,6 @@ app.get('/logout', (req, res) => {
   res.redirect('/');
 });
 
-
 //check to see if user is logged in
 app.get('/testing', (req, res)=>{
   if (req.user) {
@@ -54,9 +55,90 @@ app.get('/testing', (req, res)=>{
   }
 });
 
+//login failed
 app.get('/failed', (req, res) => {
   req.logout();
   res.redirect('/');
+});
+
+/*************************
+ ******* API STUFF *******
+ *************************/
+
+//convert latitude, longitude coordinates into city, state string
+app.put('/coordstring', (req, res) => {
+  axios.get(`https://api.openrouteservice.org/geocode/reverse?api_key=${ORS_KEY}&point.lon=${req.body.lng}&point.lat=${req.body.lat}`)
+    .then(data => {
+      const city = data.data.features[0].properties.locality;
+      const state = data.data.features[0].properties.region;
+      let place;
+      if (city) {
+        place = JSON.stringify(city + ', ' + state);
+      } else {
+        place = JSON.stringify(state);
+      }
+      //this.setState({searchInput: city + ', ' + state});
+      res.status(200).send(place);
+    })
+    .catch(error => {
+      res.status(500).send(error);
+    });
+});
+
+//Get longitude, latitude coordinates from name of city
+//WARNING: API is dumb, puts longitude first
+app.put('/getcoord', (req, res) => {
+  //console.log(JSON.stringify(req.body.str));
+  //res.send(JSON.stringify(req.body.str));
+  axios.get(`https://api.openrouteservice.org/geocode/search?api_key=${ORS_KEY}&text=${req.body.str}`)
+    .then(result => {
+      //console.log('this should be an array', res.data.features[0].geometry.coordinates);
+      res.status(200).send(result.data.features[0].geometry.coordinates);
+    })
+    .catch(err => res.status(500).send(err));
+});
+
+//Get a maximum of 25 stopping points along a given route
+app.put('/route', (req, res) => {
+  const { startLocation, endLocation } = req.body;
+  let startCoordinates, endCoordinates;
+  //res.send(JSON.stringify(startLocation + ' ==> ' + endLocation));
+  axios.get(`https://api.openrouteservice.org/geocode/search?api_key=${ORS_KEY}&text=${startLocation}`)
+    .then(start => {
+      //console.log('this should be an array', res.data.features[0].geometry.coordinates);
+      startCoordinates = start.data.features[0].geometry.coordinates;
+      //res.send(startCoordinates);
+    }).then(()=> {
+      axios.get(`https://api.openrouteservice.org/geocode/search?api_key=${ORS_KEY}&text=${endLocation}`)
+        .then(end => {
+          endCoordinates = end.data.features[0].geometry.coordinates;
+          //res.send(endCoordinates);
+        })
+        .then(() => {
+          //res.send(endCoordinates);
+          axios.get(`https://api.openrouteservice.org/v2/directions/driving-car?api_key=${ORS_KEY}&start=${startCoordinates}&end=${endCoordinates}`)
+            .then(response => {
+              //console.log(response.data);
+              //console.log(JSON.stringify(response.data.features[0].geometry.coordinates));
+              let coorArray = response.data.features[0].geometry.coordinates;
+              let placeHolder = [];
+              while (coorArray.length > 25) {
+                //console.log(`array length = ${coorArray.length}`);
+                for (let i = 0; i < coorArray.length; i += 2) {
+                  placeHolder.push(coorArray[i]);
+                }
+                coorArray = placeHolder;
+                placeHolder = [];
+              }
+              //console.log(coorArray);
+              //this.setState({ routeArray: coorArray });
+              res.send(coorArray);
+            })
+            .catch(function (error) {
+              res.send(error);
+            });
+        });
+    });
 });
 
 app.listen(PORT, (() => {
